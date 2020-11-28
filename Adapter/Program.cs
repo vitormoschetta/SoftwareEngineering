91a0b8b036c006f9689fb506ad54a45ea23ebc31
vitormoschetta <vitormoschetta@gmail.com>
2020-11-27 23:54:56 -0400

    add Adaptor

---
+++ Adapter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adaptee adaptee = new Adaptee();
+            Adaptor adaptor = new Adaptor(adaptee);
+            Client client = new Client(adaptor);
+
+            client.MetodoC();
+        }
+    }    
+
+    
+
+   
+}
