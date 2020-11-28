4e7353d35aa039733b15b7518b626ecd4013ab42
vitormoschetta <vitormoschetta@gmail.com>
2020-11-28 00:25:07 -0400

    atlz adapter

--- DesignPatterns/Adapter/Program.cs
+++ DesignPatterns/Adapter/Program.cs
@@ -2,19 +2,53 @@
 
 namespace Adapter
 {
+    public class ClasseExistente    // <-- a ser adaptada
+    {
+        public void MetodoUtil(string texto) {
+            Console.WriteLine(texto);
+        }
+    }
+
+
+    // Usando herança
+    public class AdaptadorHeranca : ClasseExistente
+    {
+        public void Operacao() {
+            MetodoUtil("Texto aqui");
+        }
+    }
+
+
+    // Usando composição
+    public class AdaptadorComposicao 
+    {
+        private ClasseExistente _existente;
+        public AdaptadorComposicao(ClasseExistente existente)
+        {
+            _existente = existente;
+        }
+
+        public void Operacao() {
+            _existente.MetodoUtil("Texto aqui");
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Adaptee adaptee = new Adaptee();
-            Adaptor adaptor = new Adaptor(adaptee);
-            Client client = new Client(adaptor);
+            // usando herança
+            AdaptadorHeranca adaptadorHeranca = new AdaptadorHeranca();
+            adaptadorHeranca.Operacao();
 
-            client.MetodoC();
+
+            // usando composição
+            ClasseExistente existente = new ClasseExistente();
+            AdaptadorComposicao adaptadorComposicao = new AdaptadorComposicao(existente);
+            adaptadorComposicao.Operacao();
         }
     }    
 
     
-
    
 }
