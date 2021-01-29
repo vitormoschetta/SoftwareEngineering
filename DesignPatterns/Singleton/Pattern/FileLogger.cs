05e5be8d730a2f79623a8740f7ce4d46d89454ef
vitormoschetta <vitormoschetta@gmail.com>
2021-01-29 00:59:04 -0400

    read

--- DesignPatterns/Singleton/Pattern/FileLogger.cs
+++ DesignPatterns/Singleton/Pattern/FileLogger.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 namespace Pattern
 {
     public class FileLogger
-    {                
+    {
+        private FileLogger() { }
         private static FileLogger _instance;
         public string Text { get; set; }
 
         public static FileLogger GetInstance()
         {
-            if (_instance == null)            
+            if (_instance == null)
                 _instance = new FileLogger();
-            
+
             return _instance;
         }
 
--- DesignPatterns/Singleton/Pattern/Program.cs
+++ DesignPatterns/Singleton/Pattern/Program.cs
@@ -11,7 +11,19 @@ namespace Pattern
             Console.WriteLine(log01.Text);
 
             var log02 = FileLogger.GetInstance();            
-            Console.WriteLine(log02.Text); // <-- Era pra ser nulo o log02.Text caso fosse instanciado.
+            Console.WriteLine(log02.Text);
+
+
+            /*
+            Para garantir que haja apenas uma instancia da classe FileLogger tornamos privado o Construtor.
+            Logo, não é possível executar o código abaixo:
+                var log = new FileLogger();
+
+            Definimos o método 'GetInstance' o meio pelo qual FileLogger será instanciado. 
+            No corpo deste método verificamos se já existe uma instancia da classe. Em caso
+            negativo, intanciamos. Em caso positivo, retornamos a instancia já existente.
+
+            */
         }
     }
 }
